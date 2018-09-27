9911cf4bace4067a7b281da0976e714918422aac
lilakitty <1530031@upv.edu.mx>
2018-09-26 22:43:18 -0500

    Examen parte 2
    Joomla

---
+++ joomla/administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-09-26 21:03:37 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-09-26T21:03:37+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-09-26T21:30:35+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-09-26T21:35:04+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-09-26T21:35:10+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-09-27T02:31:25+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
